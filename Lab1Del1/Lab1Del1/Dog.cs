966eaf0d418503eb402e83299fc644d513818427
Pelsen85 <pelsen85@gmail.com>
2016-10-12 11:40:52 +0200

    Add files via upload
    First time

---
+++ Lab1Del1/Lab1Del1/Dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1Del1
+{
+    class Dog
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string Breed { get; set; }
+
+
+        public string WriteDogs()
+        {
+            return String.Format("Here are your dogs{0} {1} {2}", Name, Age, Breed);
+        }
+    }   
+}
