bfaebcad5182e83099326d4b253327a72380ee2e
Pelsen85 <pelsen85@gmail.com>
2016-11-16 13:59:33 +0100

    Add files via upload

---
+++ Labb14/Labb14/Classes/Publication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb14.Classes
+{
+    class Publication
+    {
+        public string Title { get; set; }
+
+        public Author Author { get; set; }
+        public int ReleaseDate { get; set; }
+    }
+}
---
+++ Labb14/Labb14/Runtime.cs
@@ -0,0 +1,51 @@
+﻿using Labb14.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb14
+{
+    class Runtime
+    {
+        public void Start()
+        {
+            Book myBook = new Book
+            {
+                Title = "Blessing",
+                Author = new Author { Name = "John", Age = 45},
+                Genre = "Fantasy",
+                Pages = 214,
+                ReleaseDate = 1978
+
+            };
+
+            Magazine myMagazine = new Magazine
+            {
+                Title = "Hustler",
+                Author = new Author { Name = "Larry Flint", Age = 80},
+                Genre = "Erotic",
+                Pages = 23,
+                ReleaseDate = 2014
+            };
+
+            Paper myPaper = new Paper
+            {
+                Title = "Aqua",
+                Author = new Author { Name = "Anna", Age =27},
+                Genre = "Essay",
+                Pages = 12,
+                ReleaseDate = 2016
+            };
+
+            Author author1 = myBook.Author;
+
+
+            Author author2 = myMagazine.Author;
+
+            Author author3 = myPaper.Author;
+
+    }
+    }
+}
