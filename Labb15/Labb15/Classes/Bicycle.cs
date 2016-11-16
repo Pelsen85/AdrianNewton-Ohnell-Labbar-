00db33895165e1960bad9c6ae46ec2ab16c94fd4
Pelsen85 <pelsen85@gmail.com>
2016-11-16 14:21:37 +0100

    Add files via upload

---
+++ Labb15/Labb15/Classes/Bicycle.cs
@@ -0,0 +1,30 @@
+﻿using Labb15.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb15.Classes
+{
+    class Bicycle : IVehicle
+    {
+        public void Start()
+        {
+            Console.WriteLine("Step on the pedals");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("Stop stepping on the pedals");
+        }
+        public void Lock()
+        {
+            Console.WriteLine("Locking bicycle");
+        }
+
+        public void Unlock()
+        {
+            Console.WriteLine("Unlocking bicycle");
+        }
+    }
+}
---
+++ Labb15/Labb15/Classes/Spaceship.cs
@@ -0,0 +1,31 @@
+﻿using Labb15.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb15.Classes
+{
+    class Spaceship : IVehicle
+    {
+        public void Start()
+        {
+            Console.WriteLine("Hyperthrusters starting");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("Hyperthrusters stopping");
+        }
+
+        public void Lock()
+        {
+            Console.WriteLine("Locking spacelock");
+        }
+
+        public void Unlock()
+        {
+            Console.WriteLine("Unlocking spacelock");
+        }
+    }
+}
