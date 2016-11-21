d2f7ce6b9ee7264efb9ad73c97f2885e0d5739b6
Pelsen85 <pelsen85@gmail.com>
2016-11-21 21:05:21 +0100

    Add files via upload

---
+++ Labb9/Labb9/GUI.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Labb9
+{
+    class GUI
+    {
+        public void GraficGrid(char[] arr)
+        {
+            Console.WriteLine("********************");
+            Console.WriteLine("*     |     |      *");
+            Console.WriteLine("*  {0}  |  {1}  |  {2}   *", arr[1], arr[2], arr[3]);
+            Console.WriteLine("*_____|_____|_____ *");
+            Console.WriteLine("*     |     |      *");
+            Console.WriteLine("*  {0}  |  {1}  |  {2}   *", arr[4], arr[5], arr[6]);
+            Console.WriteLine("*_____|_____|_____ *");
+            Console.WriteLine("*     |     |      *");
+            Console.WriteLine("*  {0}  |  {1}  |  {2}   *", arr[7], arr[8], arr[9]);
+            Console.WriteLine("*     |     |      *");
+            Console.WriteLine("********************");
+        }
+
+        public void PlayerTurn(int player)
+        {
+            GraficTopView();
+            if (player % 2 == 1)//checking the chance of the player  
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Player 1 Chance");
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Player 2 Chance");
+            }
+            Console.WriteLine("\n");
+        }
+
+        private void GraficTopView()
+        {
+            Console.WriteLine("*****************************");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Player1: X ");
+            Console.ResetColor();
+            Console.Write("******");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(" Player2: O");
+            Console.ResetColor();
+            Console.WriteLine("*****************************\n");
+        }
+
+        internal void ErrorMessage(int choice, char arr)
+        {
+            Console.WriteLine("Sorry the row {0} is already marked with {1}", choice, arr);
+            Console.WriteLine("\n");
+            Console.WriteLine("Please wait 2 second board is loading again.....");
+            Thread.Sleep(2000);
+        }
+
+        internal void PlayerWin(char[] arr, int player)
+        {
+            if ((player % 2) + 1 == 1)
+            {
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                GraficGrid(arr);// getting filled board again
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine("Player {0} has won", (player % 2) + 1);
+                    Thread.Sleep(700);
+                    ClearOneLine();
+                    Thread.Sleep(500);
+                }
+                Console.WriteLine("Player {0} has won", (player % 2) + 1);
+                Thread.Sleep(200);
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                GraficGrid(arr);// getting filled board again
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine("Player {0} has won", (player % 2) + 1);
+                    Thread.Sleep(700);
+                    ClearOneLine();
+                    Thread.Sleep(500);
+                }
+                Console.WriteLine("Player {0} has won", (player % 2) + 1);
+                Thread.Sleep(200);
+                Console.ResetColor();
+            }
+        }
+
+        internal void PlayerDraw(char[] arr, int player)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            GraficGrid(arr);// getting filled board again
+            Console.WriteLine("Draw");
+            Console.ResetColor();
+        }
+
+        internal void BackOrExit()
+        {
+            var runtime = new Runtime();
+            Console.WriteLine("***************************");
+            Console.WriteLine("*   1. Start Game again   *");
+            Console.WriteLine("*   2. Exit               *");
+            Console.WriteLine("***************************");
+            var input = Console.ReadKey(true).Key;
+            switch (input)
+            {
+                case ConsoleKey.NumPad1:
+                case ConsoleKey.D1:
+
+                    break;
+                case ConsoleKey.NumPad2:
+                case ConsoleKey.D2:
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+
+        public int TakeChoice()//Taking users choice 
+        {
+
+            int x = 0;
+            bool loop = true;
+            while (loop)
+            {
+                string input = Console.ReadLine();
+                if (input == "1")
+                {
+                    x = Convert.ToInt16(input);
+                    loop = false;
+                }
+                if (input == "2")
+                {
+                    x = Convert.ToInt16(input);
+                    loop = false;
+                }
+                if (input == "3")
+                {
+                    x = Convert.ToInt16(input);
+                    loop = false;
+                }
+                if (input == "4")
+                {
+                    x = Convert.ToInt16(input);
+                    loop = false;
+                }
+                if (input == "5")
+                {
+                    x = Convert.ToInt16(input);
+                    loop = false;
+                }
+                if (input == "6")
+                {
+                    x = Convert.ToInt16(input);
+                    loop = false;
+                }
+                if (input == "7")
+                {
+                    x = Convert.ToInt16(input);
+                    loop = false;
+                }
+                if (input == "8")
+                {
+                    x = Convert.ToInt16(input);
+                    loop = false;
+                }
+                if (input == "9")
+                {
+                    x = Convert.ToInt16(input);
+                    loop = false;
+                }
+                else
+                    Console.WriteLine("Choose between 1-9");
+                Thread.Sleep(1000);
+                ClearOneLine();
+                ClearOneLine();
+            }
+
+            return x;
+        }
+
+        void ClearOneLine()
+        {
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+        }
+    }
+}
---
+++ Labb9/Labb9/Runtime.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb9
+{
+    class Runtime
+    {
+        IBoard board = new Board();
+        GUI gui = new GUI();
+        int choice;
+        public static int flag { get; set; } = 0;
+
+        public void Start()
+        {
+            Node grid = new Node();
+            int player = grid.Player = 1;
+            char[] mark = grid.Mark;
+
+            do
+            {
+                Console.Clear();
+                GraficGrid(mark, player);
+                choice = gui.TakeChoice();
+                board.PlaceMarker(choice, mark, player);// calling the PlaceMarker Function  
+                player++;
+                Console.ResetColor();
+            } while (flag != 1 && flag != -1);// This loop will be run until all cell of the grid is not marked with X and O or some player is not win
+            GameOver(mark, flag, player);
+        }
+
+
+
+
+        void GraficGrid(char[] mark, int player)
+        {
+            gui.PlayerTurn(player);
+            gui.GraficGrid(mark);
+        }
+
+        public static int CheckWin(char[] arr)
+        {
+
+            #region Horzontal Winning Condtion
+            //Winning Condition For First Row   
+            if (arr[1] == arr[2] && arr[2] == arr[3])
+            {
+                return 1;
+            }
+            //Winning Condition For Second Row   
+            else if (arr[4] == arr[5] && arr[5] == arr[6])
+            {
+                return 1;
+            }
+            //Winning Condition For Third Row   
+            else if (arr[7] == arr[8] && arr[8] == arr[9])
+            {
+                return 1;
+            }
+            #endregion
+
+            #region vertical Winning Condtion
+            //Winning Condition For First Column       
+            else if (arr[1] == arr[4] && arr[4] == arr[7])
+            {
+                return 1;
+            }
+            //Winning Condition For Second Column  
+            else if (arr[2] == arr[5] && arr[5] == arr[8])
+            {
+                return 1;
+            }
+            //Winning Condition For Third Column  
+            else if (arr[3] == arr[6] && arr[6] == arr[9])
+            {
+                return 1;
+            }
+            #endregion
+
+            #region Diagonal Winning Condition
+            else if (arr[1] == arr[5] && arr[5] == arr[9])
+            {
+                return 1;
+            }
+            else if (arr[3] == arr[5] && arr[5] == arr[7])
+            {
+                return 1;
+            }
+            #endregion
+
+            #region Checking For Draw
+            // If all the cells or values filled with X or O then any player has won the match  
+            else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
+            {
+                return -1;
+            }
+            #endregion
+
+            else
+            {
+                return 0;
+            }
+        }
+
+        void GameOver(char[] arr, int flag, int player)
+        {
+            Console.Clear();// clearing the console  
+            var counter = new Counter() { WinWin = flag };
+            var counter2 = new Counter() { DrawDraw = flag };
+            counter.WinMessage += (sender, e) =>
+            {
+                gui.PlayerWin(arr, player);
+                gui.BackOrExit();
+            };
+            counter2.DrawMessage += (sender, e) =>
+            {
+                gui.PlayerDraw(arr, player);
+                gui.BackOrExit();
+            };
+            if (counter.WinWin == 1)
+            {
+                counter.OnWinnigHappend(this, EventArgs.Empty);
+            }
+            else if (counter.WinWin == -1)
+            {
+                counter.OnDrawHappend(this, EventArgs.Empty);
+            }
+
+
+
+
+            //if (flag==1)
+            //{                
+            //    gui.PlayerWin(arr, player);
+            //    gui.BackOrExit();
+            //}
+
+            //else// if flag value is -1 the match will be draw and no one is winner  
+            //{
+            //    gui.PlayerDraw(arr, player);
+            //    gui.BackOrExit();
+            //}
+            Console.ReadLine();
+
+        }
+
+    }
+}
